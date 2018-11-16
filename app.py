db43c05a28cc35d18014a132d64fabaafbcd1cb3
Igor Derkach <gosha753951@gmail.com>
2018-11-16 16:30:32 +0300

    Make app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import flask
+import os
+from flask_pymemcache import FlaskPyMemcache
+
+app = flask.Flask('test_wg')
+
+# You can override config file via CONFIG_FILE environment var
+config_fn = os.environ.get('CONFIG_FILE') or 'config/prod.py'
+app.config.from_pyfile(config_fn)
+
+memcache = FlaskPyMemcache()
+memcache.init_app(app)
