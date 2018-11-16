03725163950a7447adf067aac28fcd65b86a0eda
Igor Derkach <gosha753951@gmail.com>
2018-11-16 19:19:33 +0300

    Fix config file name in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_pymemcache import FlaskPyMemcache
 app = flask.Flask('test_wg')
 
 # You can override config file via CONFIG_FILE environment var
-config_fn = os.environ.get('CONFIG_FILE') or 'config/prod.py'
+config_fn = os.environ.get('CONFIG_FILE') or 'config/default_config.py'
 app.config.from_pyfile(config_fn)
 
 memcache = FlaskPyMemcache()
